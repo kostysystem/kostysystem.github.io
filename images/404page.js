669af60d2230bea61829d9ef9c304c552d302002
Про компы <155679909+prokompi@users.noreply.github.com>
2024-07-14 22:17:29 +0300

    Update 404page.js

--- images/404page.js
+++ images/404page.js
@@ -1,6 +1,6 @@
-var h3 = document.getElementById(".h1");
+var h3 = document.getElementById("h1");
 h3.innerHTML = "Файл не найден";
-var p1 = document.getElementById(".p1");
+var p1 = document.getElementById("p1");
 p1.innerHTML = "Упс! Этого файла не существует!";
-var p2 = document.getElementById(".p2");
+var p2 = document.getElementById("p2");
 p2.innerHTML = "Возможно URL этого файла указан неверно! Проверьте правильность URL-адреса или перейдите на главную.";
