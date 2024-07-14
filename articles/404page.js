84e4413177d16b778de7d282555f535ac22bce6b
Про компы <155679909+prokompi@users.noreply.github.com>
2024-07-14 22:24:52 +0300

    Create 404page.js

---
+++ articles/404page.js
@@ -0,0 +1,6 @@
+var h3 = document.getElementById("h1");
+h3.innerHTML = "Статья не найдена";
+var p1 = document.getElementById("p1");
+p1.innerHTML = "Упс! Этой статьи не существует! Возможно она появится в ближайшем будущем!";
+var p2 = document.getElementById("p2");
+p2.innerHTML = "Возможно URL указан неверно! Проверьте правильность URL-адреса. Вы так-же можете перейти к списку статей или на главную.";
