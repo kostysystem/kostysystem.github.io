8a6ef98422527f768fb52def18a0bb8a0508b6a2
Про компы <155679909+prokompi@users.noreply.github.com>
2024-07-14 17:29:39 +0300

    Update 404page.js

--- images/404page.js
+++ images/404page.js
@@ -1,1 +1,3 @@
-window.location.href = 'https://prokompi.github.io/';
+document.getElementById("err1404").innerHTML = "Файл не найден";
+document.getElementById("p1404").innerHTML = "Упс! Этого файла не существует!";
+document.getElementById("p2404").innerHTML = "Возможно URL этого файла указан неверно! Проверьте правильность URL-адреса или перейдите на главную.";
