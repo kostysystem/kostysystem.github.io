987e6c34a7d344a1ded02f2966dfd50a485cbee4
Про компы <155679909+prokompi@users.noreply.github.com>
2024-07-14 21:51:57 +0300

    Update 404page.js

--- images/404page.js
+++ images/404page.js
@@ -1,3 +1,6 @@
+nfScript();
+function nfScript(){
 document.getElementById("err1404").innerHTML = "Файл не найден";
 document.getElementById("p1404").innerHTML = "Упс! Этого файла не существует!";
 document.getElementById("p2404").innerHTML = "Возможно URL этого файла указан неверно! Проверьте правильность URL-адреса или перейдите на главную.";
+};
