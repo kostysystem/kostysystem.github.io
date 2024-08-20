da249c613242979df609a9ba2f2ce25dc4678780
KostySystem <155679909+kostysystem@users.noreply.github.com>
2024-08-20 16:51:46 +0300

    Add files via upload

---
+++ local.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+	const o = ymaps.geolocation;
+    var country = o.country
+    if (country == "Россия" || country == "Беларусь"){
+var countrycontent = "ruby"
+var content = document.getElementById("local-content");
+content.innerHTML = "<br><hr><br><h2>Отечественное ПО</h2><br><p>В России начался переход на Отечественное ПО. Чаще всего это касается организаций. Российские ОС чаще всего основаны на ядре Linux, поэтому скоро на нашем сайте будут выходить статьи про Linux.</p> <h2><a href=\"special/perexod-ru-po.html\">Подробнее</a><br><hr><br><br>";
+
+    } else {
+var countrycontent = "other"
+    }
+}
---
+++ special/local.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+	const o = ymaps.geolocation;
+    var country = o.country
+    if (country == "Россия" || country == "Беларусь"){
+var countrycontent = "ruby"
+document.title = "Переход на отечественное ПО - KostySystem";
+var content = document.getElementById("page");
+content.innerHTML = "<center><br><img src=\"favicon.ico\" width=\"70\" height=\"70\"><h1>KostySystem</h1><h3>Сайт про компьютеры.</h3><hr><br><br><br><h3>Мы начинаем выпускать статьи по Linux с 15 сентября 2024.</h3><p>В России начался переход на Отечественное ПО. Это пока что касается только организаций. Обычных пользователей это пока не касается. Российские ОС чаще всего основаны на ядре Linux, поэтому скоро на нашем сайте будут выходить статьи про Linux. Статьи по Linux так-же будут полезны тем, кто хочет перейти с Windows на Linux. Инструкции будут работать как на отечественных, так и на других дистрибутивах.</p> <br> <h2><a href=\"/\">На главную сайта</a><br><br><br><br><br><br><br><br><br>";
+
+    } else {
+var countrycontent = "other"
+document.title = "Что-то пошло не так - KostySystem";
+var content = document.getElementById("page");
+content.innerHTML = "<center><br><img src=\"favicon.ico\" width=\"70\" height=\"70\"><h1>KostySystem</h1><h3>Ошибка</h3><hr><br><h3>Что-то пошло не так...</h3><br><h4>Страница недоступна. Увы.</h4><h4>Вы можете отправиться на главную.</h4><br><br><h1><a href=\"https://kostysystem.github.io\">На главную</a></h1><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br></center>";
+    }
+}
