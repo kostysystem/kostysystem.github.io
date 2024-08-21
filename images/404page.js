47850a90a94710d45c90a10072ff150876f502ed
KostySystem <155679909+kostysystem@users.noreply.github.com>
2024-08-21 21:57:22 +0300

    Delete images/404page.js

--- images/404page.js
+++
@@ -1,7 +0,0 @@
-document.title = "Файл не найден - Про Компы";
-var h3 = document.getElementById("h1");
-h3.innerHTML = "Файл не найден";
-var p1 = document.getElementById("p1");
-p1.innerHTML = "Упс! Этого файла не существует!";
-var p2 = document.getElementById("p2");
-p2.innerHTML = "Возможно URL этого файла указан неверно! Проверьте правильность URL-адреса или перейдите на главную.";
