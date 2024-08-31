38caaad840e07772dd31a7951ea6c75dc316d868
KostySystem <155679909+kostysystem@users.noreply.github.com>
2024-08-31 23:59:03 +0300

    Update elements.js

--- elements.js
+++ elements.js
@@ -5,20 +5,20 @@ var copyright = document.getElementById("copyright");
 copyright.innerHTML = "<br>  <h2><img src=\"/favicon.ico\" width=\"70\" height=\"70\" align=\"center\"> <span class=\"sitename-12812\">KostySystem </span></h2><h4 class=\"element-12812\">Сайт про компьютеры</h4> <br> <center> <h3>Навигация</h3><br><p><a href=\"/\" class=\"bottom-element\">Главная</a></p> <p><a href=\"/index.html#aboutus\" class=\"bottom-element\">О нас</a></p> <p><a href=\"/newbie/index.html\" class=\"bottom-element\">Для новичков</a></p> <p><a href=\"/articles/index.html\" class=\"bottom-element\">Статьи</a> </p> <p> <a href=\"/blog/index.html\" class=\"bottom-element\">Блог</a></center></p><br><br> <div class=\"avtor-pravo\"><h5>© KostySystem, 2024</h5></div> <br>";
 
 var menu = document.getElementById("menu");
-menu.innerHTML = "<div id=\"notewrite\"> </div><div id=\"notewriteerror\"> </div><div class=\"zakrep\"><nav>     <div class=\"pcnav\"><ul class=\"topmenu\">    <li><h2> <img src=\"/favicon.ico\" width=\"40px\" height=\"40px\" align=\"center\"> KostySystem</h2></li>    <li> </li>    <li> </li>    <li> </li>    <li> </li>    <li> </li>    <li><a href=\"/\">Главная</a></li>    <li><a href=\"/newbie/index.html\">Для новичков</a></li>    <li><a href=\"/articles/index.html\">KostySystem - статьи</a></li>    <li><a href=\"/blog/index.html\">Блог</a></li>  </ul></div>       <div class=\"menu\"><br>    <input type=\"checkbox\" id=\"burger-checkbox\" class=\"burger-checkbox\">    <label for=\"burger-checkbox\" class=\"burger\"></label>   <ul class=\"menu-list\"><br><br><center>            <li><a class=\"menu-item\" href=\"/\">Главная</a></li>            <li><a class=\"menu-item\" href=\"/newbie/index.html\">Для новичков</a></li>            <li><a class=\"menu-item\" href=\"/articles/index.html\">KostySystem - статьи</a></li>            <li><a class=\"menu-item\" href=\"/blog/index.html\">Блог</a></li>       </ul><br></div></div><span class=\"textphone\"> <img src=\"/favicon.ico\" width=\"35px\" height=\"35px\" align=\"center\"> <span class=\"bold-mobile\">KostySystem</span></span> </nav>";
+menu.innerHTML = "<div id=\"notewrite\"> </div><div id=\"notewriteerror\"> </div><div class=\"zakrep\"><nav>     <div class=\"pcnav\"><ul class=\"topmenu\">    <li><h2> <img src=\"/favicon.ico\" width=\"40px\" height=\"40px\" align=\"center\"> KostySystem</h2></li>    <li> </li>    <li> </li>    <li> </li>    <li> </li>    <li> </li>    <li><a href=\"/\">Главная</a></li>    <li><a href=\"/newbie/index.html\">Для новичков</a></li>    <li><a href=\"/articles/index.html\">KostySystem - статьи</a></li>    <li><a href=\"/blog/index.html\">Блог</a></li>  </ul></div>       <div class=\"menu\"><br>    <input type=\"checkbox\" id=\"burger-checkbox\" class=\"burger-checkbox\">    <label for=\"burger-checkbox\" class=\"burger\"></label>   <ul class=\"menu-list\"><br><br><center>            <li><a class=\"menu-item\" href=\"/\">Главная</a></li>            <li><a class=\"menu-item\" href=\"/newbie/index.html\">Для новичков</a></li>            <li><a class=\"menu-item\" href=\"/articles/index.html\">KostySystem - статьи</a></li>            <li><a class=\"menu-item\" href=\"/blog/index.html\">Блог</a></li>       </ul><br></div></div><span class=\"textphone\"> <img src=\"/favicon.ico\" width=\"35px\" height=\"35px\" align=\"center\"> <span class=\"bold-mobile\">KostySystem</span></span> </nav><br>";
 // ============================================================================
 
 // Украшательства
 
 // ==========================
 // Тех-работы
-var tech = "true"
-var prichinazakritia = "Мы работаем над новым обновлением для нашего веб-сайта, в результате чего сайт временно недоступен!"
+var tech = "false"
+var prichinazakritia = "unknown"
 var kogdadostypno = "01.09.2024 в 00:00"
 
 // Надпись сверху страницы
 var note = "false"
-var notetext = "Нас ждёт обновление! <a href=\"/blog/2.html\" class=\"hyberlink\">Подробнее</a>"
+var notetext = "Message <a href=\"#\" class=\"hyberlink\">Подробнее</a>"
 
 var type = "normal"
 // error - сообщение о баге, сбое или проблеме.
