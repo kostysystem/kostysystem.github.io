d63a9a498267051bb69af7d11e378797d6ee00c8
KostySystem <155679909+kostysystem@users.noreply.github.com>
2024-09-22 16:24:59 +0300

    Update local.js

--- geoblock/local.js
+++ geoblock/local.js
@@ -2,9 +2,8 @@ window.onload = function () {
 	const o = ymaps.geolocation;
     var country = o.country
     if (country == "Украина"){
-var country = "blocked"
+var rabotaet = "no"
     } else {
-var country = "unblocked"
 document.body.innerHTML = "Сайт доступен!";
 window.location.hash = "#"
 window.location.pathname = "/"
